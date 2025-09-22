0dbb4055a5975615c64e370b4dbc0d07b2c99ea2
ali heydarianfard <alihf74@gmail.com>
2025-09-23 00:26:16 +0330

    add test api

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -1,18 +1,37 @@
-// home-component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-
+export interface Business {
+  name: string;
+  categoryName: string;
+  businessCategory: number;
+  address: {
+    provinceName: string;
+    cityName: string;
+    fullAddress: string;
+    postalCode: string;
+  };
+}
 @Component({
   selector: 'app-home-component',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home-component.html',
-  styleUrl: './home-component.scss'
+  styleUrls: ['./home-component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  businesses: Business[] = [];
 
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  ngOnInit(): void {
 
-
+    this.http.get<Business[]>('dev.api.timedari.ir/api/Business/all')
+      .subscribe({
+        next: (data) => this.businesses = data,
+        error: (err) => console.error('API error:', err)
+      });
+  }
 }
