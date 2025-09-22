5ba1c4f59c55743207506420f657c9b5de5d8dea
ali heydarianfard <alihf74@gmail.com>
2025-09-22 12:14:04 +0330

    chnage

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -3,26 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
 
-interface Business {
-  name: string;
-  category: number;
-  ownerId: string;
-  status: number;
-  address: {
-    city: string;
-    street: string;
-    postalCode: string;
-  };
-  location: {
-    latitude: number;
-    longitude: number;
-  };
-  services: any[];
-  schedules: any[];
-  id: string;
-  createdAt: string;
-  updatedAt: string | null;
-}
+
 
 @Component({
   selector: 'app-home-component',
@@ -31,15 +12,9 @@ interface Business {
   styleUrls: ['./home-component.scss']
 })
 export class HomeComponent implements OnInit {
-  businesses: Business[] = [];
+
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-    this.http.get<Business[]>('https://dev.api.timedari.ir/api/Business/all')
-      .subscribe({
-        next: (res) => this.businesses = res,
-        error: (err) => console.error('API call failed', err)
-      });
-  }
+
 }
